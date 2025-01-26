10b6677d88bea5c198eb9253115e313b591dcbb6
shrish <adhikari.shrish1@gmail.com>
2025-01-26 20:14:58 +0545

    Added basic router and mounting

---
+++ Makefile
@@ -0,0 +1,3 @@
+# install that module
+install_dot_env:
+	go install github.com/joho/godotenv/cmd/godotenv@latest
---
+++ main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/cors"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	godotenv.Load()
+
+	portString := os.Getenv("PORT")
+	if portString == "" {
+		log.Fatal("PORT must be specified")
+	}
+
+	router := chi.NewRouter()
+
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins: []string{"https://*", "http://*"},
+		AllowedMethods: []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders: []string{"*"},
+		ExposedHeaders: []string{"Link"},
+		MaxAge:         300,
+	}))
+	srv := &http.Server{
+		Handler: router,
+		Addr:    ":" + portString,
+	}
+
+	v1Router := chi.NewRouter()
+	v1Router.HandleFunc("/healthz", handlerReadiness)
+
+	router.Mount("/v1", v1Router)
+
+	log.Printf("Server starting on port %v", portString)
+	// This is blocking and only returns if error happens
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
